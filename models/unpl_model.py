8758bf1cf2da9895c1fa0e4b45e275a7097406a4
fun3sx <106584775+fun3sx@users.noreply.github.com>
2022-06-20 17:20:16 +0300

    added unemployment functionality

---
+++ models/unpl_model.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 12:17:14 2022
+
+@author: User
+"""
+
+from dataapi1.database import db
+
+class UNPLModel(db.Model):
+	month = db.Column(db.String(100), nullable=False,primary_key=True)
+	value = db.Column(db.String(100), nullable=False)
+
+	def __repr__(self):
+		return f"Entry(month = {month}, value = {value})"
